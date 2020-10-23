3ea2d2862726a7b98028daa8d17f7342aef08a17
Celio Eduardo <celioeduardo@me.com>
2020-10-23 19:22:29 -0300

    Install kubectl

--- src/main/bash/custom/pipeline-k8s.sh
+++ src/main/bash/custom/pipeline-k8s.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x 
+
 function logInToPaas() {
     local clusterName="PAAS_${ENVIRONMENT}_CLUSTER_NAME"
 	local k8sClusterName="${!clusterName}"
@@ -7,6 +9,17 @@ function logInToPaas() {
     local clusterRegion="PAAS_${ENVIRONMENT}_CLUSTER_REGION"
     local k8sClusterRegion="${!clusterRegion}"
 
+    echo "Path to kubectl [${KUBECTL_BIN}]"
+	if [[ "${TEST_MODE}" == "false" && "${KUBECTL_BIN}" != "/"* ]]; then
+		echo "Downloading CLI"
+		curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/${SYSTEM}/amd64/kubectl" --fail
+		KUBECTL_BIN="$(pwd)/${KUBECTL_BIN}"
+	fi
+	chmod +x "${KUBECTL_BIN}"
+	echo "Removing current Kubernetes configuration from [${KUBE_CONFIG_PATH}]"
+	rm -rf "${KUBE_CONFIG_PATH}" || echo "Failed to remove Kube config. Continuing with the script"
+
+    echo "logInToPass aws region: ${k8sClusterRegion:-us-east-1} name ${k8sClusterName}"
     aws eks --region ${k8sClusterRegion:-us-east-1} update-kubeconfig --name ${k8sClusterName}
 }
 
